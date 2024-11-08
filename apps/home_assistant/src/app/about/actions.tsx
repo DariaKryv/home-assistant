df7257c8fd2a61606f77509c1db881f8a21fcb1d
Daria Kryvosheieva <Daria.Kryvosheieva@ordina.nl>
2024-11-08 16:08:41 +0100

    feat: fixed the button code

--- apps/home_assistant/src/app/about/actions.tsx
+++ apps/home_assistant/src/app/about/actions.tsx
@@ -1,14 +1,15 @@
 'use server';
 
-export const toggle = () => {
-  fetch(
+export const toggle = async (state: string) => {
+  const x = await fetch(
     'http://192.168.178.202:8123/api/states/switch.shellyplusplugs_d48afc5ada8c_switch_0',
     {
       headers: {
         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiZWJlNWVmMDEyYTU0YjQ2YjlkYzQzMmZjZjM0NDllYSIsImlhdCI6MTcyNzQ0Njg3MiwiZXhwIjoyMDQyODA2ODcyfQ.rDG4pTb4mzwxBzZ-MHq64WoGZ7MstybeISwfOOHJm3o`,
       },
       method: 'post',
-      body: JSON.stringify({ state: 'on' }),
+      body: JSON.stringify({ state: state === 'on' ? 'off' : 'on' }),
     }
   );
+  return x.json();
 };
--- apps/home_assistant/src/app/about/toggle.tsx
+++ apps/home_assistant/src/app/about/toggle.tsx
@@ -2,6 +2,7 @@
 
 import { toggle } from './actions';
 import '../css/toggleButton.css';
+import { useState } from 'react';
 
 interface Props {
   entity: {
@@ -13,17 +14,21 @@ interface Props {
 }
 
 export const Toggle = ({ entity }: Props) => {
+  const [isOn, setIsOn] = useState(entity.state);
   const entityInfo = `${entity.attributes.friendly_name}(${entity.state})`;
+  const handleToggle = async () => {
+    const x = await toggle(isOn);
+    setIsOn(x.state);
+    console.log(x);
+  };
   return (
     <div>
       <button
         className="toggle-button"
-        onClick={() => {
-          toggle();
-        }}
+        onClick={handleToggle}
         title={entityInfo}
       >
-        On
+        {isOn === 'on' ? 'Off' : 'On'}
       </button>
     </div>
   );
